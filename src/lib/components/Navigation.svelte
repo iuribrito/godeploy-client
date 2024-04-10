516d480bd8c566fa4a701643475887fd65086496
Iuri Brito <iuri.brito@gmail.com>
2024-04-10 08:58:01 -0300

    mark menu item active page

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,14 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import NavigationItem from "./NavigationItem.svelte";
+
+  const items = [
+    { href: "/", label: "Home" },
+    { href: "/projects", label: "Projects" },
+  ];
+</script>
+
 <div class="hidden lg:ml-6 lg:block">
   <div class="flex space-x-4">
-    <a
-      href="/"
-      class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white hover:text-white hover:bg-blue-600"
-      >Dashboard</a
-    >
-    <a
-      href="/projects"
-      class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-blue-600 hover:text-white"
-      >Projects</a
-    >
+    {#each items as { href, label }}
+      {#if $page.url.pathname === href}
+        <NavigationItem {label} {href} current />
+      {:else}
+        <NavigationItem {label} {href} />
+      {/if}
+    {/each}
   </div>
 </div>
---
+++ src/lib/components/NavigationItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let label: string;
+  export let href: string;
+
+  export let current = false;
+
+  let className =
+    "rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white hover:bg-blue-600";
+
+  if (current) {
+    className =
+      "rounded-md px-3 py-2 text-sm font-medium text-white bg-blue-600";
+  }
+</script>
+
+<a {href} class={className}>
+  {label}
+</a>
